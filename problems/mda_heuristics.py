7d8eb8f09a3a2dfbbdbd6f2954ba42c946e28db8
ofir511996 <ofir511996@gmail.com>
2020-11-23 13:53:44 +0200

    until Q38 23/11

--- problems/mda_heuristics.py
+++ problems/mda_heuristics.py
@@ -186,11 +186,8 @@ class MDATestsTravelDistToNearestLabHeuristic(HeuristicFunction):
                            get_air_distance_between_junctions(junction, lab.location),
                            self.problem.problem_input.laboratories))  # TODO: replace `...` with the relevant implementation.
 
-        total_dist = 0.
         nr_tests = state.get_total_nr_tests_taken_and_stored_on_ambulance()
-        if nr_tests != 0:
-            total_dist += (air_dist_to_closest_lab(state.current_location) *
-                           nr_tests)
+        total_dist = air_dist_to_closest_lab(state.current_location) * nr_tests
         for apart in self.problem.get_reported_apartments_waiting_to_visit(state):
             total_dist += apart.nr_roommates * air_dist_to_closest_lab(apart.location)
         return total_dist
